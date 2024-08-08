b36e62fb8a9001561d6af18079882770c87e3f74
Nayelipadron24 <nayelii.jaqui.p.v@gmail.com>
2024-08-08 10:55:03 -0600

    Se inicio con las importaciones e iconos que se van a utilizar para realizar la app

---
+++ contador_pasos/lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pedometer/pedometer.dart';
+import 'package:decimal/decimal.dart';
+
+void main() => runApp(new MyApp());
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context){
+    return new Container();
+  }
+}
